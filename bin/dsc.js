877f1c208849270454b030b3f3c9d688a2c0df05
chenzhigang <979703986@qq.com>
2022-02-28 19:42:03 +0800

    subcode

--- bin/dsc.js
+++ bin/dsc.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+'use strict'
+
+const { program } = require('commander')
+const { version } = require('../package.json')
+
+program
+    .version(version)
+
+console.log('version => ', version)
