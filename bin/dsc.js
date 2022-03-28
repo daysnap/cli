0d5fdbce2b0e46151a98ee3a1544733a8b47f8b3
chenzhigang <979703986@qq.com>
2022-03-28 22:22:07 +0800

    refactor: 创建项目 50%

--- bin/dsc.js
+++ bin/dsc.js
@@ -27,8 +27,10 @@ program
     .usage('<app-name> -t <template>')
     .option('-t, --template <template>', '指定项目模板')
     .option('-l, --list', '当前的模板列表')
-    .option('-o, --offline', '使用缓存模板')
-    .option('-u, --username <username>', '指定组织或用户')
+    .option('-c, --cache', '使用线下缓存模板')
+    .option('-o, --org <orgname>', '指定组织')
+    .option('-d, --deposit <deposit>', '指明使用 gitee 还是 github')
+    .option('-u, --username <username>', '指定用户')
     .action(createRouter(require('../lib/create')))
 
 // husky 辅助
