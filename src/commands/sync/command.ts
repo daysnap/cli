5481d389760116f06bf11f18c5ad492b34639698
chenzhigang <979703986@qq.com>
2023-02-22 15:13:25 +0800

    feat: sync

--- src/commands/sync/command.ts
+++ src/commands/sync/command.ts
@@ -4,7 +4,7 @@ export default createCommand((ctx) => {
   ctx.program
     .command('sync [names...]')
     .description(
-      '同步更新 npm 包到淘宝源，解决发布之后，淘宝源没有立即更新的问题',
+      '同步更新 npm 包到淘宝源，解决发布之后，淘宝源没有立即更新的问题。默认同步执行目录下 package.json 的 name 包.',
     )
     .action(
       createRouter(ctx, requireAll(requireContext(__dirname, /\/on.*\.js$/))),
--- src/commands/sync/onDefalut.ts
+++ src/commands/sync/onDefalut.ts
@@ -1,22 +1,52 @@
 import { createRoute } from '@/core'
-import { isExists } from '@/utils'
+import { isExists, spinner } from '@/utils'
+import fetch from 'node-fetch'
+import { sleep } from '@daysnap/utils'
+
+const sync = async (name: string) => {
+  const url = `https://registry-direct.npmmirror.com/${name}/sync?publish=false&nodeps=false`
+  const { logId }: { ok: boolean; logId: string } = await fetch(url, {
+    method: 'put',
+  }).then((res) => res.json())
+  await showState(name, logId)
+}
+
+const showState = async (name: string, logId: string) => {
+  const url = `https://registry-direct.npmmirror.com/${name}/sync/log/${logId}?offset=0`
+  const { log }: { ok: string; log: string } = await fetch(url).then((res) =>
+    res.json(),
+  )
+
+  spinner.text = `同步 ${name} 状态：${log}`
+  if (log && log.includes('[done] Sync')) {
+    return await sleep(1000)
+  }
+
+  return new Promise<void>((resolve) => {
+    setTimeout(async () => {
+      await showState(name, logId)
+      resolve()
+    }, 1000)
+  })
+}
 
 export default createRoute(async (ctx) => {
-  const { configServer, args } = ctx
-  const [names] = args
+  const names: string[] = ctx.args[0]
 
   if (!names.length) {
     const pkgPath = `${process.cwd()}/package.json`
     if (await isExists(pkgPath)) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const { name } = require(pkgPath)
-      names.push(name)
+      names.push(require(pkgPath).name)
     }
     if (names.length === 0) {
       throw new Error('未指定需要同步的 npm 包名')
     }
   }
 
-  // 需要同步的
-  console.log('args => ', names)
+  spinner.start()
+  for (let i = 0, len = names.length; i < len; i++) {
+    await sync(names[i])
+  }
+  spinner.succeed(`${names.join(' ')} 同步完成`)
 })
