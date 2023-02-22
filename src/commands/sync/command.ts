59a2e3995cd04c1f66c5617a0e69fdd99a32ab9f
chenzhigang <979703986@qq.com>
2023-02-22 09:41:27 +0800

    chore: command

--- src/commands/sync/command.ts
+++ src/commands/sync/command.ts
@@ -2,7 +2,7 @@ import { createCommand, createRouter, requireAll, requireContext } from '@/core'
 
 export default createCommand((ctx) => {
   ctx.program
-    .command('sync [pkgs...]')
+    .command('sync [names...]')
     .description(
       '同步更新 npm 包到淘宝源，解决发布之后，淘宝源没有立即更新的问题',
     )
--- src/commands/sync/onDefalut.ts
+++ src/commands/sync/onDefalut.ts
@@ -1,7 +1,22 @@
 import { createRoute } from '@/core'
+import { isExists } from '@/utils'
 
 export default createRoute(async (ctx) => {
   const { configServer, args } = ctx
-  const [pkgs] = args
-  console.log('args => ', args)
+  const [names] = args
+
+  if (!names.length) {
+    const pkgPath = `${process.cwd()}/package.json`
+    if (await isExists(pkgPath)) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { name } = require(pkgPath)
+      names.push(name)
+    }
+    if (names.length === 0) {
+      throw new Error('未指定需要同步的 npm 包名')
+    }
+  }
+
+  // 需要同步的
+  console.log('args => ', names)
 })
