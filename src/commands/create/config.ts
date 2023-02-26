dc7ae603b628d4a5f698b331d7685a363b4a561f
chenzhigang <979703986@qq.com>
2023-02-26 22:29:36 +0800

    feat: create

--- src/commands/create/config.ts
+++ src/commands/create/config.ts
@@ -1,3 +1,23 @@
-export interface Config {}
+export interface Config {
+  orgname: string
+  cache: boolean
+  deposit: string
+  depositUrls: Record<string, any>
+  repoUrls: Record<string, any>
+}
 
-export const config: Config = {}
+export const config: Config = {
+  orgname: 'daysnap-templates',
+  cache: false,
+  deposit: 'github',
+  depositUrls: {
+    github: `https://api.github.com/users/{username}/repos?page=1&per_page=999`,
+    gitee: `https://gitee.com/api/v5/{type}/{username}/repos?page=1&per_page=999`,
+  },
+  repoUrls: {
+    // github: 'github:{username}/{repo}',
+    // gitee: 'gitee:{username}/{repo}',
+    github: 'https://github.com/{username}/{repo}.git',
+    gitee: 'https://gitee.com/{username}/{repo}.git',
+  },
+}
