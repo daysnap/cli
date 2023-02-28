7fbc9ef2a8081fffb44e8a643d11999bedabd744
chenzhigang <979703986@qq.com>
2023-02-28 13:08:29 +0800

    chore: generate

--- src/commands/create/generate/index.ts
+++ src/commands/create/generate/index.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import { getOptions } from './options'
 import { askQuestions } from './ask'
 import { filterFiles } from './filter'
+import { renderTemplate } from '@/commands/create/generate/render'
 
 export const generate = async (options: {
   src: string
@@ -15,6 +16,7 @@ export const generate = async (options: {
   metalsmith
     .use(askQuestions(opts.configureInquirer || opts.prompts, { name }))
     .use(filterFiles(opts.configureFilter || opts.filters, { name }))
+    .use(renderTemplate(opts.skipInterpolation || opts.filters))
 
   return new Promise<void>((resolve, reject) => {
     metalsmith
--- src/commands/create/generate/render.ts
+++ src/commands/create/generate/render.ts
@@ -1,9 +1,39 @@
 import Metalsmith from 'metalsmith'
+import multimatch from 'multimatch'
 import consolidate from 'consolidate'
 import { isString } from '@daysnap/utils'
+import { MetalsmithHandleOptions } from './types'
 
-export const render = async () => {
-  //
+const handlebarsRender = (str: string, data: Record<string, any>) =>
+  new Promise<string>((resolve, reject) => {
+    consolidate.handlebars.render(str, data, (err, res) => {
+      err ? reject(err) : resolve(res)
+    })
+  })
+
+export const render = async (
+  skipInterpolation: string | string[],
+  options: Pick<MetalsmithHandleOptions, 'metalsmith' | 'files'>,
+) => {
+  const { metalsmith, files } = options
+  const metadata = metalsmith.metadata()
+  const fileNames = Object.keys(files)
+  for (let i = 0, len = fileNames.length; i < len; i++) {
+    const fileName = fileNames[i]
+    if (
+      skipInterpolation &&
+      multimatch([fileName], skipInterpolation, { dot: true }).length
+    ) {
+      continue
+    }
+    const file = files[fileName]
+    const str = file.contents.toString()
+    if (!/{{([^{}]+)}}/g.test(str)) {
+      continue
+    }
+    const content = await handlebarsRender(str, metadata)
+    file.contents = new Buffer(content)
+  }
 }
 
 export const renderTemplate = (
@@ -13,6 +43,12 @@ export const renderTemplate = (
     skipInterpolation = [skipInterpolation]
   }
   return (files, metalsmith, done) => {
-    // consolidate.handlebars.render()
+    render(skipInterpolation, { files, metalsmith })
+      .then(() => {
+        done(null, files, metalsmith)
+      })
+      .catch((err) => {
+        done(err, files, metalsmith)
+      })
   }
 }
