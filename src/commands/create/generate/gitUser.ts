7187df5c7345be739330b175175c219405f41d7f
chenzhigang <979703986@qq.com>
2023-03-11 23:02:16 +0800

    fix: eslint error

--- src/commands/create/generate/gitUser.ts
+++ src/commands/create/generate/gitUser.ts
@@ -7,7 +7,9 @@ export const getGitUser = () => {
   try {
     name = exec('git config --get user.name')
     email = exec('git config --get user.email')
-  } catch {}
+  } catch {
+    //
+  }
 
   name = name && JSON.stringify(name.toString().trim()).slice(1, -1)
   email = email && ' <' + email.toString().trim() + '>'
